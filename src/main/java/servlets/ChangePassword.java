721aff4cbb8d23e68f3c5c8a6e641bd8ba6af046
Sumeet Yajnik <sumeet.yajnik@ellucian.com>
2021-12-26 15:05:47 +0530

    Initial commit

---
+++ src/main/java/servlets/ChangePassword.java
@@ -0,0 +1,28 @@
+package main.java.servlets;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.*;
+
+public class ChangePassword extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter out = resp.getWriter();
+        String pass = req.getParameter("passwordEntered");
+        if (!Login.isLoggedIn){
+            out.println("You must login first");
+        }
+        else if (pass.equals("")){
+            out.println("Password can't be empty");
+        }
+        else if (Login.isLoggedIn && !pass.equals("")){
+            Login.password = pass;
+            out.println("Password changed. New Password is "+Login.password);
+        }
+        else {
+            out.println("Sorry, Something went wrong");
+        }
+        out.close();
+    }
+}
